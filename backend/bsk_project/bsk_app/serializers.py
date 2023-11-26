a041ffd2ed8286b065fa717626cae651491d23aa
fullmoon03 <borem03@gmail.com>
2023-11-26 17:56:22 +0900

    회원가입

--- backend/bsk_project/bsk_app/serializers.py
+++ backend/bsk_project/bsk_app/serializers.py
@@ -1,19 +1,40 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from .models import FlowerPot, UserProfile
+from django.contrib.auth import get_user_model
+from .models import FlowerPot, ProfileImage
+
+
+UserProfile = get_user_model()
 
 class FlowerPotSerializer(serializers.ModelSerializer):
     class Meta:
         model = FlowerPot
         fields = '__all__'
 
-class UserProfileSerializer(serializers.ModelSerializer):
+class ProfileImageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserProfile
+        model = ProfileImage
         fields = '__all__'
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    profile_picture = ProfileImageSerializer(required=False)
+    flower_pot = FlowerPotSerializer(required=False)
 
-class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ['korean_name', 'phone_number', 'password', 'profile_picture', 'flower_pot_number']
+        fields = ('id', 'username', 'korean_name', 'profile_picture', 'flower_pot', 'notifications_enabled', 'nickname')
+        # 보안 상의 이유로 password 제외 - signup 혹은 endpoint(비번 변경) 등의 상황에서만 예외적으로
+
+class SignUpSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ('username', 'password', 'korean_name', 'profile_picture', 'flower_pot', 'notifications_enabled', 'nickname')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- backend/bsk_project/bsk_app/urls.py
+++ backend/bsk_project/bsk_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import FlowerPotListCreateView, UserProfileListCreateView, SignupView
+from .views import SignUpView
 
 urlpatterns = [
-    path('flowerpots/', FlowerPotListCreateView.as_view(), name='flowerpot-list-create'),
-    path('userprofiles/', UserProfileListCreateView.as_view(), name='userprofile-list-create'),
-    path('signup/', SignupView.as_view(), name='signup'),
+    # path('flowerpots/', FlowerPotListCreateView.as_view(), name='flowerpot-list-create'),
+    # path('userprofiles/', UserProfileListCreateView.as_view(), name='userprofile-list-create'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
