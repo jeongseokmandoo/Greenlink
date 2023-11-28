90b5e289300ecb0dc8470009fbed98bac051b545
jeongseokmandoo <127060327+jeongseokmandoo@users.noreply.github.com>
2023-11-28 15:48:46 +0900

    group1

--- frontend/src/components/AccountBox.js
+++ frontend/src/components/AccountBox.js
@@ -1,13 +1,37 @@
 import { ProfileImg2 } from "./ProfileImg";
 import React from "react";
-import styles from "./AccountBox.module.css";
 
-function AccountBox({ src, peoplename, nickname }) {
+const style = {
+  // display: "inline-flex",
+  width: "100vw",
+};
+
+const contentstyle = {
+  diplay: "flex",
+  flexDirection: "column",
+};
+
+const namestyle = {
+  fontSize: "2.5em",
+  fontWeight: "800",
+  marginTop: "1vh",
+};
+
+const nicknamestyle = {
+  fontSize: "1.5em",
+  fontWeight: "600",
+  marginTop: "0.5vh",
+  opacity: "40%",
+};
+
+function AccountBox({ className, src, peoplename, nickname }) {
   return (
-    <div className={styles.accountbox}>
+    <div className={className} style={style}>
       <ProfileImg2 src={src} alt="프로필 이미지" />
-      <h5>{peoplename}</h5>
-      <p>{nickname}</p>
+      <div style={contentstyle}>
+        <div style={namestyle}>{peoplename}</div>
+        <div style={nicknamestyle}>{nickname}</div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/GroupPage1.js
+++ frontend/src/pages/GroupPage1.js
@@ -10,8 +10,9 @@ import Profileimg2 from "../assets/Setting_Icon.png";
 import Profileimg3 from "../assets/Gallery_Icon.png";
 import Profileimg4 from "../assets/Home_Icon.png";
 import { useState } from "react";
+import styles from "./GroupPage1.module.css";
 
-function GroupPage1(props) {
+function GroupPage1() {
   const [isInvited, setisInvited] = useState(false);
   const handleInviteFamily = () => {
     setisInvited(!isInvited);
@@ -27,16 +28,36 @@ function GroupPage1(props) {
         icon1={no_icon}
         icon2={setting_icon}
       />
-      <AccountBox
-        src={Profileimg1}
-        peoplename="이윤금"
-        nickname="이 집 최고 권력자"
-      />
-      <Btn2 text="내 프로필 수정" link="/group2" />
-      <h4>연결된 가족</h4>
-      <AccountBox src={Profileimg2} peoplename="박지한" nickname="귀염둥이" />
-      <AccountBox src={Profileimg3} peoplename="박하영" nickname="재롱둥이" />
-      <AccountBox src={Profileimg4} peoplename="윤동하" nickname="막내" />
+      <div>
+        <AccountBox
+          className={styles.accountbox}
+          src={Profileimg1}
+          peoplename="이윤금"
+          nickname="이 집 최고 권력자"
+        />
+        <Btn2 text="내 프로필 수정" link="/group2" />
+        <div className={styles.graybox}></div>
+        <h4>연결된 가족</h4>
+        <AccountBox
+          className={styles.accountbox}
+          src={Profileimg2}
+          peoplename="박지한"
+          nickname="귀염둥이"
+        />
+        <AccountBox
+          className={styles.accountbox}
+          src={Profileimg3}
+          peoplename="박하영"
+          nickname="재롱둥이"
+        />
+        <AccountBox
+          className={styles.accountbox}
+          src={Profileimg4}
+          peoplename="윤동하"
+          nickname="막내"
+        />
+      </div>
+      <div className={styles.graybox}></div>
       <button onClick={handleInviteFamily}>가족 초대하기</button>
       {isInvited && <p>화분번호</p>}
       <MainNav />
